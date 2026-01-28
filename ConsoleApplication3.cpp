292c45a9df34be0ddba5c3f4e9857f7eac557b04
ialikh72 <ialikh.w1@gmail.com>
2026-01-28 20:55:46 +0500

    Add project files.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int abc = 4, b = 1997;
+	float xy = 3.4;
+	char name[14] = "Abdul Sami";
+	cout << name << endl;
+	cout << abc << endl;
+	cout << b << endl;
+	cout <<xy << endl;
+
+
+}
